d3d072f723c82672f3b1c909f9cf3fc4ec34f8f8
Mihaela Mesarosiu <mihaela@kokoon.dev>
2020-09-24 19:19:48 +0300

    Added comments

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,5 +1,13 @@
 const commandIndentation = '&nbsp;&nbsp;&nbsp;&nbsp;';
 
+/**
+ * Recursive function which flattens a command block which describes
+ * an instruction with one or more subcommands.
+ *
+ * @param {Object} item Command block
+ * @param {number} offset Number of command indentations to prepend
+ * @param {Array} acc An array with the accumulation of commands with indentation
+ */
 export const flattenCommandBlock = (item, offset, acc) => {
   acc.push(`${commandIndentation.repeat(offset)}${item.command}`);
   if (item.block) {
@@ -9,6 +17,13 @@ export const flattenCommandBlock = (item, offset, acc) => {
   }
 };
 
+/**
+ * Takes an algorithm described by one or many command blocks and flattens
+ * it in an array with indentation formatted commands.
+ *
+ * @param {Array} algo A list of command blocks which describe an algorithm
+ * @returns {Array} A list of objects with formatted instructions.
+ */
 export const flattenAlgorithm = (algo) => {
   let commands = [];
   for (const commandBlock of algo) {
